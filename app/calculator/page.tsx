22102d3e954828754900885111259efe93fb97e5
SV3N77 <danny.jiang1234@hotmail.com>
2023-07-13 14:07:42 +1000

    update cards and calculator page

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -1,3 +1,11 @@
 export default function Page() {
-  return <div>page</div>;
+  return (
+    <section className="container mx-auto mt-10 flex flex-col">
+      <h1 className="text-xl">Calculator</h1>
+      <section className="flex flex-col gap-4 py-6">
+        <div className="">Input</div>
+        <div className="">Numbers</div>
+      </section>
+    </section>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ type CardProps = { title: string; url: string };
 export default function Home() {
   return (
     <main className="container mx-auto mt-10 flex flex-col">
-      <section className="mt-4 flex flex-col gap-4 py-4 lg:grid lg:grid-cols-2">
+      <section className="mx-auto flex w-3/5 flex-col gap-4 py-6 lg:grid lg:grid-cols-2">
         <Cards title="Calculator" url="/calculator" />
         <Cards title="Naughts and Crosses" url="/naughtsandcrosses" />
       </section>
@@ -17,7 +17,7 @@ export default function Home() {
 function Cards({ title, url }: CardProps) {
   return (
     <Link href={url}>
-      <div className="flex flex-col overflow-hidden rounded-lg bg-violet-100 p-4 shadow-lg transition hover:-translate-y-4 md:h-20 md:flex-row">
+      <div className="flex flex-col justify-end overflow-hidden rounded-lg bg-violet-100 p-4 shadow-lg transition hover:-translate-y-4 md:h-16 md:flex-row">
         <h1 className="text-lg">{title}</h1>
       </div>
     </Link>
