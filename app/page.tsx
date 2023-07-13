20134712598c8b0c04290b4e5eea97380b5277c0
SV3N77 <danny.jiang1234@hotmail.com>
2023-07-13 11:11:51 +1000

    update cards in home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
+
+type CardProps = { title: string; url: string };
+
 export default function Home() {
   return (
-    <main className="p-4">
-      <div className="flex flex-wrap">Calculator</div>
+    <main className="container mx-auto mt-10 flex flex-col">
+      <section className="mt-4 flex flex-col gap-4 py-4 lg:grid lg:grid-cols-2">
+        <Cards title="Calculator" url="/calculator" />
+        <Cards title="Naughts and Crosses" url="/naughtsandcrosses" />
+      </section>
     </main>
+  );
+}
+
+// internal components
+function Cards({ title, url }: CardProps) {
+  return (
+    <Link href={url}>
+      <div className="flex flex-col overflow-hidden rounded-lg bg-violet-100 p-4 shadow-lg transition hover:-translate-y-4 md:h-20 md:flex-row">
+        <h1 className="text-lg">{title}</h1>
+      </div>
+    </Link>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-blue-300 p-5 ">
-      <nav className="container mx-auto flex flex-col items-center justify-center gap-4 text-lg text-white md:flex-row">
-        <h1 className="grow text-3xl font-semibold">Random Stuff</h1>
+    <header className="bg-blue-300 p-5">
+      <nav className="container mx-auto flex flex-col gap-4 text-lg text-white md:flex-row">
+        <Link href="/">
+          <h1 className="grow text-3xl font-semibold">Random Stuff</h1>
+        </Link>
       </nav>
     </header>
   );
