bb7dfff6a958f7f346ab482b4dc89d8bcd6f9e81
SV3N77 <danny.jiang1234@hotmail.com>
2023-07-14 13:02:40 +1000

    update calculator

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -1,9 +1,9 @@
 export default function Page() {
   return (
-    <section className="container mx-auto mt-10 flex flex-col">
+    <section className="container mx-auto mt-10 flex flex-col gap-4">
       <h1 className="text-xl">Calculator</h1>
-      <section className="flex flex-col gap-4 py-6">
-        <div className="">Input</div>
+      <section className="mx-auto flex w-3/5 flex-col gap-4 rounded-md bg-zinc-400 px-4 py-6">
+        <div className="rounded-md bg-slate-200 p-4 text-right">Input</div>
         <div className="">Numbers</div>
       </section>
     </section>
